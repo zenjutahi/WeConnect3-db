e3d1ed6a840c6b96ca2a350f642931317674f60b
zenjutahi <zenjutahi@gmail.com>
2018-05-14 14:15:32 +0300

    setup a view helper function

--- app/view_helpers.py
+++ app/view_helpers.py
@@ -1,4 +1,5 @@
 import datetime
+import uuid
 from flask import request, jsonify
 from flask_jwt_extended import create_access_token, decode_token
 from app.utils import check_email
@@ -10,6 +11,12 @@ def validate_email(email):
     if not check_email(email):
         return False
     return True
+
+def generate_string(string_length=10):
+    """Generate a string for password"""
+    random = str(uuid.uuid4())
+    random = random.replace("-", "")
+    return random[:string_length]
 
 
 def token_generator(current_user, expire_time=datetime.timedelta(minutes=30)):
