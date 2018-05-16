504ce563bbf1983ffa6374e3bbe515d1b7bb839b
zenjutahi <zenjutahi@gmail.com>
2018-05-16 13:27:52 +0300

    edit require json function

--- app/utils.py
+++ app/utils.py
@@ -12,7 +12,7 @@ def require_json(f):
         # Check for json
         if not check_json():
             return jsonify(
-                {'message':'Bad Request. Request should be JSON format'}), 405
+                {'message':'Bad Request. Request should be JSON format'}), 400
         ret_val =  f(*args, **kwargs)
         return ret_val
     return wrapper
