ee9a17d995053b152633cfa580127c2512b7e63d
zenjutahi <zenjutahi@gmail.com>
2018-05-16 10:43:22 +0300

    added filter and search functions

--- app/utils.py
+++ app/utils.py
@@ -4,7 +4,6 @@ from flask import request, jsonify
 from functools import wraps
 
 # from app import mail
-
 def require_json(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
@@ -14,8 +13,11 @@ def require_json(f):
         if not check_json():
             return jsonify(
                 {'message':'Bad Request. Request should be JSON format'}), 405
-        return f(*args, **kwargs)
+        ret_val =  f(*args, **kwargs)
+        return ret_val
     return wrapper
+
+
 
 def check_json():
     """Returns True if request is json"""
@@ -23,8 +25,42 @@ def check_json():
         return False
     return True
 
-def business_search(query):
-    pass
+def business_search(search_params):
+    search = search_params.get('q')
+    operator = "%" + search + "%"
+    subquery = Business.query.filter(Business.name.ilike(operator))
+                            # .filter(Business.description.ilike(operator))\
+                            # .filter(Business.category.ilike(operator))\
+                            # .filter(Business.location.ilike(operator))
+
+    limit = search_params.get('limit')
+    page = search_params.get('page')
+    subquery = business_pagination(subquery, limit, page)
+    return subquery.all()
+
+def business_filter(filter_params):
+    """" A filter algorithim """
+    subquery=Business.query
+    required_fields = ['location', 'name', 'category']
+    for filter in filter_params:
+        if filter not in required_fields:
+            continue
+        operator = "%" + filter_params[filter] + "%"
+        subquery = subquery.filter(getattr(Business, filter).ilike(operator))
+
+
+    limit = filter_params.get('limit')
+    page = filter_params.get('page')
+    subquery = business_pagination(subquery, limit, page)
+    return subquery.all()
+
+def business_pagination(subquery, limit, page):
+    """ a pagination function """
+    limit = int(limit or 2)
+    page = int(page or 1)
+    offset = (page - 1)*limit
+    subquery = subquery.limit(limit).offset(offset)
+    return subquery
 
 
 
@@ -38,7 +74,7 @@ def check_email(email):
 def validate_auth_data_null(data):
     """Returns data if input is valid else none"""
     pattern = r'^[a-zA-Z\d_ ]+[\d\w]{3,}'
-    match = re.search(pattern, data)
+    match = re.search(pattern, str(data))
     if not match:
         return None
     else:
@@ -57,5 +93,5 @@ def check_blank_key(data, required_fields):
 
     for field in required_fields:
         if not data.get(field):
-            assert 0, field + ' is Missing'
+            assert 0, field + ' should not be missing'
     return data
