09e8ddfbc31af16da66a3c18dc1bf6b0e4c9800f
zenjutahi <zenjutahi@gmail.com>
2018-05-22 05:24:30 +0300

    edit message responces

--- app/auth/views.py
+++ app/auth/views.py
@@ -9,6 +9,7 @@ from app.models import User, TokenBlacklist
 from app.view_helpers import validate_email, token_generator, generate_string
 from app.utils import validate_auth_data_null, check_blank_key, require_json
 
+
 @auth.route('/register', methods=['POST'])
 @require_json
 def register():
@@ -26,21 +27,20 @@ def register():
 
     if not username or not email or not password:
         return jsonify(
-                {'message':'You need email, username and password to register'}), 400
+            {'message': 'You need email, username and password to register'}), 400
 
     if not validate_email(email):
         return jsonify(
-                {'message':'Invalid Email. Enter valid email to register'}), 400
+            {'message': 'Invalid Email. Enter valid email to register'}), 400
     existant_user = User.query.filter_by(email=email).first()
     if existant_user:
         return jsonify(
-                {'message': 'This email is registered, login instead'}), 404
-    new_user = User(email=email, username=username,first_name=first_name,
+            {'message': 'This email is registered, login instead'}), 404
+    new_user = User(email=email, username=username, first_name=first_name,
                     password=password)
     new_user.save()
     return jsonify(
         {'message': 'New user Succesfully created'}), 201
-
 
 
 @auth.route('/login', methods=['POST'])
@@ -57,15 +57,15 @@ def login():
 
     if not email or not password:
         return jsonify(
-                {'message':'Enter Valid Data: Email and password'}), 400
+            {'message': 'Enter Valid Data: Email and password'}), 400
     user = User.query.filter_by(email=email).first()
     # print('Hellow owrld', password, user.password)
     if not user:
         return jsonify(
-                {'message':'Invalid Email: Enter right credentions to login'}), 401
+            {'message': 'Invalid Email: Enter right credentions to login'}), 401
     elif not user.verify_password(password):
         return jsonify(
-                {'message':'Invalid password: Enter right password to login'}), 401
+            {'message': 'Invalid password: Enter right password to login'}), 401
     current_user = user.id
     return token_generator(current_user)
 
@@ -79,7 +79,7 @@ def logout():
     tokenlist = TokenBlacklist(token=jti, user_identity=user_identity)
     tokenlist.save()
     return jsonify(
-        {'message':'User Successfully logged out'}), 200
+        {'message': 'User Successfully logged out'}), 200
 
 
 @auth.route('/changepassword', methods=['PUT'])
@@ -96,26 +96,26 @@ def change_password():
     new_password = validate_auth_data_null(data.get('new_password'))
     jti = get_raw_jwt()['jti']
 
-    if not old_password or not new_password :
+    if not old_password or not new_password:
         return jsonify(
-                {'message':'Enter Valid Data: Email and password'}), 400
+            {'message': 'Enter Valid Data: Email and password'}), 400
     current_user = get_jwt_identity()
     user = User.query.filter_by(id=current_user).first()
     if not user.verify_password(old_password):
         return jsonify(
-                {'message':'Enter Valid Password: Old password is wrong'}), 401
+            {'message': 'Enter Valid Password: Old password is wrong'}), 401
     User.update(User, current_user, password=new_password)
     tokenlist = TokenBlacklist(token=jti, user_identity=current_user)
     tokenlist.save()
     return jsonify(
-            {'message':'Password Successfully Changed'}), 201
+        {'message': 'Password Successfully Changed'}), 201
 
 
 @auth.route('/resetpassword', methods=['POST'])
 @require_json
 def reset_password():
     try:
-        required_fields = ['email',]
+        required_fields = ['email', ]
         data = check_blank_key(request.get_json(), required_fields)
     except AssertionError as err:
         msg = err.args[0]
@@ -123,11 +123,11 @@ def reset_password():
     email = validate_auth_data_null(data.get('email'))
     if not email:
         return jsonify(
-                {'message':'Enter Valid Email'}), 400
+            {'message': 'Enter Valid Email'}), 400
     user = User.query.filter_by(email=email).first()
     if not user:
         return jsonify(
-                {'message':'Invalid Email: Enter right credentions for reset password'}), 401
+            {'message': 'Invalid Email: Enter right credentions for reset password'}), 401
     n_password = generate_string()
     User.update(User, user.id, password=n_password)
     msg = Message(subject="Weconect reset password",
@@ -136,4 +136,4 @@ def reset_password():
                   recipients=[email])
     mail.send(msg)
     return jsonify(
-            {'message':'Check your email address for new password'}), 201
+        {'message': 'Check your email address for new password'}), 201
