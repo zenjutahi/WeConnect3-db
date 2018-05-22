39d1c710ea64638615472b11c63b80683b9cc7d6
zenjutahi <zenjutahi@gmail.com>
2018-05-22 05:30:29 +0300

    adjucted responce codes

--- app/view_helpers.py
+++ app/view_helpers.py
@@ -5,12 +5,12 @@ from flask_jwt_extended import create_access_token, decode_token
 from app.utils import check_email
 
 
-
 def validate_email(email):
     """Returns True if email is valid"""
     if not check_email(email):
         return False
     return True
+
 
 def generate_string(string_length=10):
     """Generate a string for password"""
@@ -23,7 +23,7 @@ def token_generator(current_user, expire_time=datetime.timedelta(minutes=30)):
     """Returns access token and response to User"""
     response = {
         'message': 'Successfully Loged In',
-        'access_token': create_access_token(identity=current_user, expires_delta=expire_time)
-
-    }
+        'access_token': create_access_token(
+            identity=current_user,
+            expires_delta=expire_time)}
     return jsonify(response), 200
