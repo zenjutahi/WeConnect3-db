15e6cda2b9ba671e9509ab02b7c383f12863d756
zenjutahi <zenjutahi@gmail.com>
2018-05-22 05:22:39 +0300

    captured exceptions

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,7 @@ from flask_api import FlaskAPI
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token,
-    get_jwt_identity
-)
+    JWTManager)
 # local imports
 from instance.config import app_config
 # db variable initialization
@@ -27,18 +25,41 @@ def create_app(config_name):
     jwt.init_app(app)
     mail.init_app(app)
 
-    from app import models
-
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/api/auth')
 
     from .business import business as business_blueprint
-    app.register_blueprint(business_blueprint, url_prefix='/api' )
+    app.register_blueprint(business_blueprint, url_prefix='/api')
 
     from .review import review as review_blueprint
-    app.register_blueprint(review_blueprint, url_prefix='/api/businesses' )
+    app.register_blueprint(review_blueprint, url_prefix='/api/businesses')
 
     from app.models import TokenBlacklist
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        """Error handler for a bad request"""
+        return jsonify(dict(error='The Server did not understand' +
+                                  ' the request')), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        """Error handler for not found page"""
+        return jsonify(dict(error='The Resource requested is not' +
+                                    ' available')), 404
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        """Error handler for wrong method to an endpoint"""
+        return jsonify(dict(error='The HTTP request Method' +
+                                  ' is not allowed')), 405
+
+    @app.errorhandler(500)
+    def server_error(error):
+        """Error handler for a server failure"""
+        return jsonify(dict(error='The server encountered an internal error' +
+                                  ' and was unable to' +
+                                  ' complete your request')), 500
 
     @jwt.token_in_blacklist_loader
     def check_token_in_blacklist(decrypted_token):
