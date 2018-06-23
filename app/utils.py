eaf2fe90a145b4233eeb85bd27042088132ba3f3
zenjutahi <jeffmnjogu@gmail.com>
2018-06-23 14:34:16 +0300

    change missing word response message

--- app/utils.py
+++ app/utils.py
@@ -98,6 +98,6 @@ def check_blank_key(data, required_fields):
     for field in required_fields:
         if not data.get(field):
             if field == 'first_name':
-                assert 0, 'First name should not be missing'
-            assert 0, field + ' should not be missing'
+                assert 0, 'First name is required'
+            assert 0, field + ' is required'
     return data
